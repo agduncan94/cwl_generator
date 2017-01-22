32ba9431a9becee793cfdc4ec1abf6f9e0102d50
Andrew Duncan <agduncan@uwaterloo.ca>
2017-01-22 13:56:36 -0500

    More basic additions to cwl tool generator

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 angular.module('cwlgenerator', ['ui.bootstrap', 'dndLists']).controller('GeneratorCtrl', function ($scope, $log) {
 	$scope.cwlDoc = {
 		"cwlVersion": '',
+		"id": '',
 		"class": 'CommandLineTool',
 		"baseCommand": '',
 		"inputs": [],
@@ -9,16 +10,29 @@ angular.module('cwlgenerator', ['ui.bootstrap', 'dndLists']).controller('Generat
 
 	$scope.inputTypes = ["null", "boolean", "int", "long", "float", "double", "string", "File", "Directory"];
 	$scope.input = {
+		"ioType": "input",
 		"id": '',
 		"label": '',
 		"type": $scope.inputTypes[0],
 		"inputBinding": {
 			"position": 0,
-			"prefix": ''
+			"prefix": '',
+			"separate": true
 		} 
 	};
-	$scope.output = {};
+	$scope.output = {
+		"ioType": "output",
+		"id": '',
+		"label": '',
+		"type": $scope.inputTypes[0],
+		"outputBinding": {
+			"glob": ''
+		} 
+	};
 	$scope.cwlVersions = ["v1.0"];
+	$scope.inputArray = ["input"];
+	$scope.outputArray = ["output"];
+
 
 	// Initialize
 	$scope.cwlDoc.cwlVersion = $scope.cwlVersions[0];
@@ -26,25 +40,34 @@ angular.module('cwlgenerator', ['ui.bootstrap', 'dndLists']).controller('Generat
 	$scope.addInput = function() {
 		$scope.cwlDoc.inputs.push($scope.input);
 		$scope.input = {
+			"ioType": "input",
 			"id": '',
 			"label": '',
 			"type": $scope.inputTypes[0],
 			"inputBinding": {
 				"position": $scope.cwlDoc.inputs.length,
-				"prefix": ''
+				"prefix": '',
+				"separate": true
 			}
 		};
 	};
 
 	$scope.addOutput = function() {
 		$scope.cwlDoc.outputs.push($scope.output);
-		$scope.output = {};
+		$scope.output = {
+			"ioType": "output",
+			"id": '',
+			"label": '',
+			"type": $scope.inputTypes[0],
+			"outputBinding": {
+				"glob": ''
+			} 
+		};
 	};
 
 	$scope.updateInputOrder = function() {
 		for (var i = 0; i < $scope.cwlDoc.inputs.length; i++) {
 			$scope.cwlDoc.inputs[i].inputBinding.position = i;
 		}
-		console.log("updated input order!");
 	};
 });
